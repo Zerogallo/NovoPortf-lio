5bfa86e0bbf51ca23c252f9801065c1978002261
Zerogallo <167368572+Zerogallo@users.noreply.github.com>
2025-07-12 13:32:11 -0300

    up

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/NovoPortf-lio.git",
+  base: "/NovoPortf-lio",
 });
